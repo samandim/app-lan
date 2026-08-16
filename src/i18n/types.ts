7b1799adac1383aea971e7ea6dc55badaf907ae2
Mehdi Samandi <mahdi83samandi@gmail.com>
2026-08-17 00:25:00 +0330

    Clear repository

--- src/i18n/types.ts
+++
@@ -1,9 +0,0 @@
-export type InterfaceLanguage = 'en' | 'fa';
-export type LearningLanguage = 'en';
-export type SupportLanguage = 'en' | 'fa';
-
-export type TranslationKey = string;
-
-export interface TranslationDictionary {
-  [key: string]: string | TranslationDictionary;
-}
--- src/services/evaluation/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './EvaluationService';
-export * from './strategies/EvaluationStrategy';
-export * from './strategies/MultipleChoiceStrategy';
-export * from './strategies/ClozeRetrievalStrategy';
-export * from './strategies/ImplicitGrammarStrategy';
-export * from './strategies/ProductionStrategy';
-export * from './strategies/SpeakingShadowingStrategy';
